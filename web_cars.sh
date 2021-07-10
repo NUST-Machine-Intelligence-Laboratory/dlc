633426bf168fbb6c831f3bf0daaa7077459944c2
NUST-Machine-Intelligence-Laboratory <58131148+NUST-Machine-Intelligence-Laboratory@users.noreply.github.com>
2021-07-10 11:04:42 +0800

    Update web_cars.sh

--- web_cars.sh
+++ web_cars.sh
@@ -2,8 +2,8 @@
 export CUDA_VISIBLE_DEVICES=1,2
 ###step1--drop_rate 0.2 --queue_size 10 --warm_up 15
 ###step2--drop_rate 0.2 --queue_size 10 --warm_up 10
-python main.py --dataset /home/alv/Downloads/dynamic-loss-correction/data/web-car --n_classes 196 --base_lr 0.001 --batch_size 64 --epoch 100 --drop_rate 0.2 --queue_size 10 --warm_up 15 --weight_decay 1e-8 --step 1
+python main.py --root /home/alv/dynamic-loss-correction --dataset web-car --n_classes 196 --base_lr 0.001 --batch_size 64 --epoch 100 --drop_rate 0.2 --queue_size 10 --warm_up 15 --weight_decay 1e-8 --step 1
 sleep 30
-python main.py --dataset /home/alv/Downloads/dynamic-loss-correction/data/web-car --n_classes 196 --base_lr 0.0001 --batch_size 18 --epoch 100 --drop_rate 0.2 --queue_size 10 --warm_up 10 --weight_decay 1e-5 --step 2
+python main.py --root /home/alv/dynamic-loss-correction --dataset web-car --n_classes 196 --base_lr 0.0001 --batch_size 18 --epoch 100 --drop_rate 0.2 --queue_size 10 --warm_up 10 --weight_decay 1e-5 --step 2
 
 
